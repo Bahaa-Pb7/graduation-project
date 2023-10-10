e976cdba09853080d3a953fceb854209060637d4
mohamed sultan <mohammedsoltan170@gmail.com>
2023-10-10 03:24:53 +0300

    add PID without test

---
+++ project_at_pic18f4550.X/helper/PID_control/PID.c
@@ -0,0 +1,32 @@
+/* 
+ * File:   PID.h
+ * Author: mohamedsultan
+ *
+ * Created on 10 ??????, 2023, 01:42 ?
+ */
+
+
+
+#include "PID.h"
+
+
+double PID_control(PID_t *obj)
+{
+    double error= obj->target  -  obj->actual ;
+    
+    static double  prev_error=obj->target;
+    double res=0.0;
+    double P=error * obj->Kp;
+    static double I=0.0;
+    double D=(error-prev_error)*obj->Kd;
+    
+    
+    I=(I+error)*obj->Ki;
+    
+    res=I+D+P;
+    
+    
+    prev_error=error;
+    
+    return res;
+}
---
+++ project_at_pic18f4550.X/helper/PID_control/PID.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   PID.h
+ * Author: mohamedsultan
+ *
+ * Created on 10 ??????, 2023, 01:42 ?
+ */
+
+#ifndef PID_H
+#define	PID_H
+
+
+typedef struct
+{
+    
+    
+    double target ;
+     double actual ;
+    double Kp;
+     double Ki;
+     double Kd;
+}PID_t;
+double PID_control(PID_t *obj);
+
+
+
+#endif	/* PID_H */
+
